11f40866f44e7081fa176716f3980a89210bab6a
Ricky Blankenaufulland <zoolway@gmail.com>
2018-05-10 17:42:02 +0200

    change: namespace adoptions

--- AureliaBase.Web/Controllers/HomeController.cs
+++ AureliaBase.Web/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace aurelia_base.Controllers
+namespace AureliaBase.Web.Controllers
 {
     public class HomeController : Controller
     {
--- AureliaBase.Web/Program.cs
+++ AureliaBase.Web/Program.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace aurelia_base
+namespace AureliaBase.Web
 {
     public class Program
     {
